99b8bd48fe6808fcdb59dce8fcd7f44e56cf2a89
Adrian De Vera <twoadrian6@gmail.com>
2023-10-03 20:49:11 +0800

    add fortawesome

--- src/app/_providers/Theme/InitTheme/index.tsx
+++ src/app/_providers/Theme/InitTheme/index.tsx
@@ -5,7 +5,7 @@ import Script from 'next/script'
 import { defaultTheme, themeLocalStorageKey } from '../ThemeSelector/types'
 import { Nunito_Sans } from 'next/font/google'
 
-const nunito_sans = Nunito_Sans({ subsets: ['latin'] })
+const nunito_sans = Nunito_Sans({ subsets: ['latin'], weight: '200' })
 export const InitTheme: React.FC = () => {
   return (
     // eslint-disable-next-line @next/next/no-before-interactive-script-outside-document
